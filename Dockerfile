f8c1e00df56c83270e5c656333920686d8283381
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-08-11 20:48:31 -0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+#
+# Ubuntu Dockerfile
+#
+# https://github.com/dockerfile/ubuntu
+#
+
+# Pull base image.
+FROM ubuntu:20.04
+
+# Install.
+RUN \
+  sed -i 's/# \(.*multiverse$\)/\1/g' /etc/apt/sources.list && \
+  apt-get update && \
+  apt-get -y upgrade && \
+  apt-get install -y build-essential && \
+  apt-get install -y software-properties-common && \
+  apt-get install -y byobu curl git htop man unzip vim wget && \
+  rm -rf /var/lib/apt/lists/*
+
+# Set environment variables.
+ENV HOME /root
+
+# Define working directory.
+WORKDIR /root
+
+COPY docker-entrypoint.sh /usr/local/bin/
+COPY dist /app/
+COPY id.json /app/
+COPY *.sh /app/
+ENTRYPOINT ["docker-entrypoint.sh"]
+
+CMD [ "node /app/dist" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,8 @@
 sh -c "$(curl -sSfL https://release.solana.com/v1.10.32/install)"
     PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 nvm install 16 
-
 npm i -g ts-node yarn raindrops-cli
-
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar xfv *.tgz
-./ngrok config add-authtoken 1xi7CdwkB7PPYqaH9AfW1Mt8e2M_wA24a91oziAs3mBYhBXe
-tmux
-./ngrok http 3000 --hostname www.autist.design
