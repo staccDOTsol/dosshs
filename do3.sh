34e77593052e8f801c9dc8c73f7efabb0432ef83
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-08-12 03:05:50 -0300

    hi

--- do3.sh
+++ do3.sh
@@ -1,3 +1,5 @@
 
+matches-cli leave_match -d hydraintermediary -env mainnet-beta -cp ${1}
  matches-cli drain_match   -k id  -env mainnet-beta -cp ${1}
          matches-cli drain_oracle           -k id -env mainnet-beta   -cp ${1}
+
